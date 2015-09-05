da13d7f7dbd471a106fec90838a489ced8c8520c
thozh912 <thomaszhangswe@gmail.com>
2015-09-05 14:24:08 +0200

    Change alot again

--- advproginR/R/djikstra.R
+++ advproginR/R/djikstra.R
@@ -1,27 +1,25 @@
-adjecent_to_in_unvisited <- function(v1,v2,node,unvisited){
-  orderlist <- which(v1 == node)
-  neighbours <- v2[orderlist]
-  result <- neighbours[ neighbours %in% unvisited]
-  return(result)
-}
-
-edge_value <- function(num1,num2,graph){
-  stopifnot(is.data.frame(graph))
-  for(i in 1:length(graph[[1]])){
-    for(j in 1:length(graph[[2]])){
-      if(graph[[1]][i] == num1 & graph[[2]][j] == num2 & i == j){
-        place <- i
-        return(graph[[3]][i])
-      }
-    }
-  }
-  return("edge not found")
-}
-
-
-
 djikstra <- function(graph, init_node){
   stopifnot(is.data.frame(graph))
+  
+  adjecent_to_in_unvisited <- function(v1,v2,node,unvisited){
+    orderlist <- which(v1 == node)
+    neighbours <- v2[orderlist]
+    result <- neighbours[ neighbours %in% unvisited]
+    return(result)
+  }
+
+  edge_value <- function(num1,num2,graph){
+    stopifnot(is.data.frame(graph))
+    for(i in 1:length(graph[[1]])){
+      for(j in 1:length(graph[[2]])){
+        if(graph[[1]][i] == num1 & graph[[2]][j] == num2 & i == j){
+          place <- i
+          return(graph[[3]][i])
+        }
+      }
+    }
+    return("edge not found")
+  }
   dist <- rep(0,length(unique(graph[[1]])))
   resultvec <- c()
   nodevec <- c()
