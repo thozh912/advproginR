bc7fb6b206c8244bca489e63cec1cf787c3d3b20
thozh912 <thomaszhangswe@gmail.com>
2015-09-23 22:04:19 +0200

    Lots of titles and labels

--- TSA_Lab2.R
+++ TSA_Lab2.R
@@ -3,10 +3,7 @@ rm(list=ls())
 library(TSA)
 library("gridExtra")
 
-ele_con <- read.csv2("electricity_consumption.csv")
-#AR2sim takes scalar integer, vector, vector
-ele_ts <- ts(ele_con,start=c(1),freq=12)
-plot(ele_ts,main="total Swedish electricity consumption in GWh 1990-2014",xlab="Years after 1 Jan 1989",ylab="GWh")
+
 
 AR2sim_matr <- function(T,phi1,phi2){
   AR2 <- matrix(nrow = T, ncol=length(phi1))
@@ -68,29 +65,33 @@ AR2simaggregator <- function(T,phi1,phi2,sigma){
   plot1 <- hist(phi1estimates)
   plot2 <- hist(phi2estimates)
   plot3 <- hist(wnsdest)
-  sdvec <- c(sd(phi1estimates),sd(phi2estimates))
+  sdvec <- c(sd(phi1estimates-phi1),sd(phi2estimates-phi2),mean(wnsdest))
   return(sdvec)
 }
 
-#Y <- AR2sim(10000,1.6,-0.9,5)
+Y <- AR2sim(30,1.6,-0.9,5)
 
-#AR2LS(Y)
+AR2LS(Y)
 
-#arima(Y,order=c(2,0,0),include.mean=FALSE,method="ML")
-#AR2simaggregator(3000,1.6,-0.9,3)
+arima(Y,order=c(2,0,0),include.mean=FALSE,method="ML")
+AR2simaggregator(30,1.6,-0.9,5)
 
 # stationary values for AR(2) process are following
 #phi1 =c(-1.4,-0.5,0.6,1.6,0.1)
 #phi2 =c(-0.8,0.2,-0.2,-0.9,0.7)
 
+ele_con <- read.csv2("electricity_consumption.csv")
 
+ele_ts <- ts(ele_con,start=c(1),freq=12)
+#plot(ele_ts,main="total Swedish electricity consumption in GWh 1990-2014",xlab="Years after 1 Jan 1989",ylab="GWh")
 ele_model <- window(ele_ts,start=1,end=12)
 
 ele_model <- ele_model[,2]
-plot(ele_model,main="total Swedish electricity consumption in GWh 1990-2014",xlab="Years after 1 Jan 1989",ylab="GWh")
+#plot(ele_model,main="total Swedish electricity consumption in GWh 1990-2014",xlab="Years after 1 Jan 1989",ylab="GWh")
 ele_test <- window(ele_ts,start=12,end=26)
 
-# ONLY SEASONAL FIT MODEL
+# DETERMINISTIC SEASONAL FIT FOLLOWED BY ARIMA(1,0,3) MODEL
+
 month. = season(ele_model)
 
 m2 <- lm(ele_model ~ month. + time(ele_model))
@@ -100,24 +101,73 @@ resi <- rstandard(m2)
 plot(resi,type="l",main="Residuals of the seasonal model",ylab="residual",xlab="Months after 1 Jan 1990")
 points(resi,pch=as.vector(season(ele_model)))
 acf_seasonal_residuals<-acf(resi,main="Autocorrelation function of the residuals of the seasonal model",xlab="months lag",ylab="ACF",lag.max=36)
-acf_seasonal_residuals<-pacf(resi,main="Partial Autocorrelation function of the residuals of the seasonal model",xlab="months lag",ylab="PACF",lag.max=36)
+pacf_seasonal_residuals<-pacf(resi,main="Partial Autocorrelation function of the residuals of the seasonal model",xlab="months lag",ylab="PACF",lag.max=36)
 eacf(resi)
 qqnorm(resi,main=c("Quantiles of the residuals of the seasonal model","against Theoretical gaussian quantiles"), xlab="theoretical Gaussian quantiles", ylab="seasonal model residual quantiles")
 qqline(resi)
 
-BC_obj <- BoxCox.ar(ele_model[,2])
+ma4 <- arima(resi,order=c(1,0,3), seasonal = list(order = c(0,0,0)))
+fit_ar1_ma3_res <- rstandard(ma4)
 
-#DIFFLOG & SEASONAL MODEL
-difflogele <-diff(log(ele_model))
-plot(difflogele)
-month. = season(ele_model)
-month. <- month.[-length(ele_model)]
-m3 <- lm(difflogele ~ month. + time(difflogele))
-lines(ts(fitted(m3),start=c(1),freq=12),col=3)
+plot(fit_ar1_ma3_res,type="l",main="residuals of ARIMA(1,0,3) fit to the residuals of fitted seasonal model",ylab="residual",xlab="Months after 1 Jan 1990")
+acf(fit_ar1_ma3_res,main=c("Autocorrelation function of the residuals of the","residuals of ARIMA(1,0,3) fit to the residuals of fitted seasonal model"),xlab="months lag",ylab="ACF",lag.max=36)
+pacf(fit_ar1_ma3_res,main=c("Partial Autocorrelation function of the","residuals of ARIMA(1,0,3) fit to the residuals of fitted seasonal model"),,xlab="months lag",ylab="PACF",lag.max=36)
+Box.test(fit_ar1_ma3_res,type="Ljung",lag=50)
+qqnorm(fit_ar1_ma3_res,main=c("Quantiles of the residuals of ARIMA(1,0,3) fit to the residuals of fitted seasonal model",
+                              "against Theoretical gaussian quantiles"), xlab="theoretical Gaussian quantiles", ylab="ARIMA(1,0,3) fit to residuals of seasonal model residual quantiles")
+qqline(fit_ar1_ma3_res)
+tsdiag(ma4,gof.lag=36)
+eacf(fit_ar1_ma3_res,ar.max=25,ma.max=36)
 
 
-seasonaldiff<- diff(difflogele,12)
-plot(seasonaldiff)
-acf(seasonaldiff,lag.max=36)
-pacf(seasonaldiff,lag.max=36)
-Box.test(seasonaldiff,type="Ljung",lag=25)
+
+# #DIFFLOG & SEASONAL MODEL
+# difflogele <-diff(log(ele_model))
+# plot(difflogele)
+# month. = season(ele_model)
+# month. <- month.[-length(ele_model)]
+# m3 <- lm(difflogele ~ month. + time(difflogele))
+# lines(ts(fitted(m3),start=c(1),freq=12),col=3)
+# 
+# 
+# seasonaldiff<- diff(difflogele,12)
+# plot(seasonaldiff,type="l")
+# acf(seasonaldiff,lag.max=36)
+# pacf(seasonaldiff,lag.max=36)
+# eacf(seasonaldiff)
+# ma2 <- arima(seasonaldiff,order=c(2,0,3), seasonal = list(order = c(1,0,0)))
+# ma2residuals <- rstandard(m2)
+# 
+# plot(ma2residuals,type="l")
+# acf(ma2residuals)
+# pacf(ma2residuals)
+# Box.test(ma2residuals,type="Ljung",lag=50)
+# qqnorm(ma2residuals)
+# qqline(ma2residuals)
+# 
+# eacf(ma2residuals)
+
+#KISS MODEL FOUND BY ANDREA
+
+ar1 <- arima(ele_model, order = c(1,0,0), seasonal = list(order = c(1,1,0)))
+res_ar1 <- rstandard(ar1)
+
+plot(res_ar1,main="Residuals of seasonal ARIMA(1,0,0)X(1,1,0) model",ylab="residual",xlab="Years after 1 Jan 1989")
+acf(res_ar1,main="Autocorrelation function of the residuals of seasonal ARIMA(1,0,0)X(1,1,0)",xlab="years lag",ylab="ACF",lag.max=36)
+pacf(res_ar1,main="Partial Autocorrelation function of the residuals of seasonal ARIMA(1,0,0)X(1,1,0)",xlab="years lag",ylab="PACF",lag.max=36))
+hist(res_ar1)
+qqnorm(res_ar1,main=c("Quantiles of the residuals of the seasonal ARIMA(1,0,0)X(1,1,0)","against Theoretical gaussian quantiles"), xlab="theoretical Gaussian quantiles", ylab="seasonal ARIMA(1,0,0)X(1,1,0) residual quantiles")
+qqline(res_ar1)
+eacf(res_ar1,ar.max=25,ma.max=36)
+tsdiag(ar1,gof.lag=36)
+Box.test(res_ar1,type="Ljung",lag=50)
+
+# FORECASTING (under construction)
+
+pre <- predict(ar1,n.ahead = 12)
+plot(pre$pred,type="l")
+
+plot(ar1, n1 = c(1), n.ahead = 24)
+
+plot(forecast(ar1))
+
